82a1cdfcafbbc4bb97141d6bd31919ff644af9cb
Kuan Butts <kuanbutts@yahoo.com>
2016-12-08 15:39:24 -0800

    new sql and link to wiki on cc main repo and gitignore ds store

--- extract.sql
+++ extract.sql
@@ -1,4 +1,18 @@
-SELECT clients.clid, clients.cm, clients.first, clients.middle, clients.last, clients.dob, clients.otn, clients.so, cms.first AS cm_first, cms.last AS cm_last, cms.email AS cm_email
+-- this should correspond with https://github.com/slco-2016/clientcomm/wiki/County-specific-developments
+
+SELECT  clients.clid, 
+        clients.cm, 
+        clients.first,
+        coalesce(clients.middle, '') AS middle,
+        clients.last,
+        clients.dob,
+        coalesce(clients.otn, '') AS otn, 
+        coalesce(clients.so, '') AS so, 
+        cms.first AS cm_first, 
+        cms.last AS cm_last, 
+        cms.email AS cm_email 
 FROM clients
-LEFT JOIN cms ON (clients.cm = cms.cmid)
+LEFT JOIN cms ON (cms.cmid = clients.cm)
+WHERE clients.active IS TRUE
+AND cms.department IN (2, 3, 5, 6, 7, 9, 11)
 ORDER BY clients.clid;
